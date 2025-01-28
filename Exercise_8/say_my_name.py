198b4cc40bebf21a8d21a2f548d1e92d3866382c
chaitra <chaitrachubz456@gmail.com>
2025-01-28 10:17:52 +0000

    Created Exercise_8 folder and added say_my_name.py to print the full name

---
+++ Exercise_8/say_my_name.py
@@ -0,0 +1,18 @@
+# created the 2 variables first_name and last_name
+# Assign the first name to the variable 'first_name'
+first_name = "Chaitra"
+# print the value of 'first_name'
+print(first_name)
+# print the data type of 'first_name'
+print(type(first_name))
+
+# Assign the last name to the variable 'last_name'
+last_name = "Boregowda"
+print(last_name)
+print(type(last_name))
+
+# Concatenated 'first_name' and 'last_name' with a space in between and assign it to 'full_name'
+full_name = first_name + ' ' +  last_name
+# Print the value of full_name
+print(full_name)
+print(type(last_name))
